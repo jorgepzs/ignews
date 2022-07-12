6a118b3e3b5213a2c3bd07b5cae3879466f8deaa
Jorge Barros <98181223+jorgepzs@users.noreply.github.com>
2022-07-12 02:59:18 -0300

    first commit

---
+++ src/components/SigInButton/index.tsx
@@ -0,0 +1,34 @@
+import {FaGithub} from 'react-icons/fa'
+import {FiX} from 'react-icons/fi'
+
+import styles from './styles.module.scss'
+
+export function SigInButton(){
+    const isUserLoggedIn = true
+   
+    return isUserLoggedIn ? (
+        <button 
+        className={styles.SigInButton}
+            type= "button"
+        >
+           
+            <FaGithub color="#04d361"/>
+            Jorge Barros 
+            <FiX color= "#737380" className={styles.CloseIcon}/>          
+        </button>
+
+    ) :(
+        <button 
+        className={styles.SigInButton}
+            type= "button"
+        >
+           
+            <FaGithub color="#eba417"/>
+            Sing in With GitHub
+           
+        </button>
+
+    );
+
+
+}
